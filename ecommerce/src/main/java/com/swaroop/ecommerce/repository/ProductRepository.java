ffd58ef92efa76137e50702055b1bfbc783c922d
srikireddy jyothi swaroop <swaroopj861@gmail.com>
2026-03-15 16:06:15 +0530

    Add files via upload

---
+++ ecommerce/src/main/java/com/swaroop/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.swaroop.ecommerce.repository;
+
+import com.swaroop.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
